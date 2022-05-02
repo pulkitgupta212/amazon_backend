fc02aa037baf423456df97b51199bae08ddbfb43
pulkitgupta212 <pulkitgupta.rc@gmail.com>
2022-05-02 15:18:11 +0530

    signupApi

--- schema.js
+++ schema.js
@@ -4,8 +4,9 @@ const user_signup=new Schema({
 
     username:{type:String , required: true },
     email:{type:String , required: true},
+    phone:{type:String , required: true},
     password:{type:String , required: true},
-    confirmPassword:{type:String , required: true},
+    
 
 })
 
